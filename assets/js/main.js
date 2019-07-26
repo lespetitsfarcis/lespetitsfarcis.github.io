cfeaa257780792ba2b4545f8abd7a6fad06a295c
Benjamin Roth <benjamin@rubyist.fr>
2019-07-26 23:59:32 +0200

    remove carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function($) {
 
 	// loader
 	var loader = function() {
-		setTimeout(function() { 
+		setTimeout(function() {
 			if($('#ftco-loader').length > 0) {
 				$('#ftco-loader').removeClass('show');
 			}
@@ -12,28 +12,28 @@ $(document).ready(function($) {
 	};
 	loader();
 
-	var carousel = function() {
-		$('.owl-carousel').owlCarousel({
-			loop: true,
-			margin: 10,
-			nav: true,
-			stagePadding: 5,
-			nav: false,
-			navText: ['<span class="icon-chevron-left">', '<span class="icon-chevron-right">'],
-			responsive:{
-				0:{
-					items: 1
-				},
-				600:{
-					items: 2
-				},
-				1000:{
-					items: 3
-				}
-			}
-		});
-	};
-	carousel();
+	// var carousel = function() {
+	// 	$('.owl-carousel').owlCarousel({
+	// 		loop: true,
+	// 		margin: 10,
+	// 		nav: true,
+	// 		stagePadding: 5,
+	// 		nav: false,
+	// 		navText: ['<span class="icon-chevron-left">', '<span class="icon-chevron-right">'],
+	// 		responsive:{
+	// 			0:{
+	// 				items: 1
+	// 			},
+	// 			600:{
+	// 				items: 2
+	// 			},
+	// 			1000:{
+	// 				items: 3
+	// 			}
+	// 		}
+	// 	});
+	// };
+	// carousel();
 
 	// scroll
 	var scrollWindow = function() {
@@ -45,19 +45,19 @@ $(document).ready(function($) {
 
 			if (st > 150) {
 				if ( !navbar.hasClass('scrolled') ) {
-					navbar.addClass('scrolled');	
+					navbar.addClass('scrolled');
 				}
-			} 
+			}
 			if (st < 150) {
 				if ( navbar.hasClass('scrolled') ) {
 					navbar.removeClass('scrolled sleep');
 				}
-			} 
+			}
 			if ( st > 350 ) {
 				if ( !navbar.hasClass('awake') ) {
-					navbar.addClass('awake');	
+					navbar.addClass('awake');
 				}
-				
+
 				if(sd.length > 0) {
 					sd.addClass('sleep');
 				}
@@ -76,7 +76,7 @@ $(document).ready(function($) {
 	scrollWindow();
 
 	var counter = function() {
-		
+
 		$('#section-counter').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
@@ -93,22 +93,22 @@ $(document).ready(function($) {
 					  }, 7000
 					);
 				});
-				
+
 			}
 
 		} , { offset: '95%' } );
 
 	}
 	counter();
-	
-	
+
+
 
 	var contentWayPoint = function() {
 		var i = 0;
 		$('.ftco-animate').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-				
+
 				i++;
 
 				$(this.element).addClass('item-animate');
@@ -130,9 +130,9 @@ $(document).ready(function($) {
 							el.removeClass('item-animate');
 						},  k * 50, 'easeInOutExpo' );
 					});
-					
+
 				}, 100);
-				
+
 			}
 
 		} , { offset: '95%' } );
