60579600d5158119d17e456ea2ec0b7a40d43386
Amaury Peillon <apeillon@lepitch.fr>
2019-09-20 09:46:49 +0200

    Correction slideshow responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,8 +41,27 @@ $(document).ready(function ($) {
         autoplaySpeed: 1500,
         responsive: [
             {
-                breakpoint: 576,
+                breakpoint: 1024,
                 settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                     arrows: false
                 }
             }
